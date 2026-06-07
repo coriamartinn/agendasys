f577363ecaa825071b0dc8804dd7b7fddb78ec29
coria martin <coria10m@gmail.com>
2026-06-07 20:17:37 -0300

    cambios PARA SEQUELIZE

--- server/src/utils/sequelize.js
+++ server/src/utils/sequelize.js
@@ -1,7 +1,5 @@
 import { Sequelize } from "sequelize";
 
-const DB_URL = process.env.DB_URL;
-
-export const conn = new Sequelize(DB_URL, {
+export const conn = new Sequelize(process.env.DB_URL, {
   dialect: "mysql",
 });
